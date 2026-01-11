e21299f23aaf4f807d3ef596265174c1c77dfb0a
aasherkataria <aasherkataria@outlook.com>
2026-01-10 20:29:32 -0600

    initial taillark added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+      },
+      {
+        protocol: "https",
+        hostname: "html.tailus.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
